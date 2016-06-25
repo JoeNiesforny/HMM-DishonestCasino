54471c3fae98de09baa9eb1e29f366ec66208868
Joe <jozwludz@gmail.com>
2016-06-25 14:16:50 +0200

    wIP

--- HMM-DishonestCasinoApp/MainWindow.xaml.cs
+++ HMM-DishonestCasinoApp/MainWindow.xaml.cs
@@ -20,9 +20,30 @@ namespace HMM_DishonestCasinoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        static double[,] StateMatrix;
+        static double[,] ObservationMatrix;
+        static double[] InitialState;
+        static int[] ObservationSequence;
+
+        private void initializeModel()
+        {
+            StateMatrix = new double[,] { { 0.7, 0.3 }, { 0.4, 0.6 } };
+            ObservationMatrix = new double[,] { { 0.1, 0.4, 0.5 }, { 0.7, 0.2, 0.1 } };
+            InitialState = new double[] { 0.6, 0.4 };
+            ObservationSequence = new int[] { 0, 1, 0, 2 };
+        }
+
         public MainWindow()
         {
+            initializeModel();
             InitializeComponent();
+        }
+
+        private void generateModelButton_Click(object sender, RoutedEventArgs e)
+        {
+            var diceCount = int.Parse(diceCountTextBox.Text);
+            var throwCount = int.Parse(throwCountTextBox.Text);
+            var successThrowCount = int.Parse(successThrowCountTextBox.Text);
         }
     }
 }
